08b80b7673b46115e5bb5caa7c585a3feeb2b57a
Rafsan Saadi <rsaadi@mail.sfsu.edu>
2018-04-01 21:32:05 -0700

    refactored MPI files

--- particles/mpi.cpp
+++ particles/mpi.cpp
@@ -18,11 +18,11 @@ using std::set;
 #define _cutoff 0.01
 #define _density 0.0005
 
-double binSize, gridSize;
-int binCount;
+int bin_counts;
+double grid_size, bin_size, ;
 
-inline void force_compute_for_bins(vector<bin_t> &bins, int i, int j, double &dmin, double &davg, int &navg) {
-  bin_t &vec = bins[i * binCount + j];
+inline void compute_force_in_eache_bins(vector<bin_t> &bins, int i, int j, double &dmin, double &davg, int &navg) {
+  bin_t &vec = bins[i * bin_counts + j];
 
   int k = 0;
   while (k < vec.size()){
@@ -34,8 +34,8 @@ inline void force_compute_for_bins(vector<bin_t> &bins, int i, int j, double &dm
   int dx = -1;
   while(dx <= 1){
     for (int dy = -1; dy <= 1; dy++) {
-      if (i + dx >= 0 && i + dx < binCount && j + dy >= 0 && j + dy < binCount) {
-        bin_t &vec2 = bins[(i + dx) * binCount + j + dy];
+      if (i + dx >= 0 && i + dx < bin_counts && j + dy >= 0 && j + dy < bin_counts) {
+        bin_t &vec2 = bins[(i + dx) * bin_counts + j + dy];
         for (int k = 0; k < vec.size(); k++)
           for (int l = 0; l < vec2.size(); l++)
             apply_force(vec[k], vec2[l], &dmin, &davg, &navg);
@@ -46,27 +46,27 @@ inline void force_compute_for_bins(vector<bin_t> &bins, int i, int j, double &dm
 }
 
 inline void create_bins(vector<bin_t> &bins, particle_t *particles, int n) {
-  gridSize = sqrt(n * _density);
-  binSize = _cutoff;
-  binCount = int(gridSize / binSize) + 1;
+  grid_size = sqrt(n * _density);
+  bin_size = _cutoff;
+  bin_counts = int(grid_size / bin_size) + 1;
 
-  bins.resize(binCount * binCount);
+  bins.resize(bin_counts * bin_counts);
 
   int i = 0;
   while (i < n){
-    int x = int(particles[i].x / binSize);
-    int y = int(particles[i].y / binSize);
-    bins[x * binCount + y].push_back(particles[i]);
+    int x = int(particles[i].x / bin_size);
+    int y = int(particles[i].y / bin_size);
+    bins[x * bin_counts + y].push_back(particles[i]);
 
     i++;
   }
 }
 
 void push_particles_in_bin(particle_t &particle, vector<bin_t> &bins) {
-  int x = particle.x / binSize;
-  int y = particle.y / binSize;
+  int x = particle.x / bin_size;
+  int y = particle.y / bin_size;
 
-  bins[x * binCount + y].push_back(particle);
+  bins[x * bin_counts + y].push_back(particle);
 }
 
 
@@ -75,8 +75,8 @@ inline void calculate_neighbor_particles(int i, int j, vector<int> &neighbors) {
     for (int dy = -1; dy <= 1; dy++) {
       if (dx == 0 && dy == 0)
         continue;
-      if (i + dx >= 0 && i + dx < binCount && j + dy >= 0 && j + dy < binCount) {
-        int index = (i + dx) * binCount + j + dy;
+      if (i + dx >= 0 && i + dx < bin_counts && j + dy >= 0 && j + dy < bin_counts) {
+        int index = (i + dx) * bin_counts + j + dy;
         neighbors.push_back(index);
       }
     }
@@ -147,16 +147,12 @@ int main(int argc, char **argv) {
   delete[] particles;
   particles = NULL;
 
-  int x_bins_per_proc = binCount / n_proc;
-
-  // although each worker has all particles, we only access particles within
-  // my_bins_start, my_bins_end.
-
-  int my_bins_start = x_bins_per_proc * rank;
-  int my_bins_end = x_bins_per_proc * (rank + 1);
+  int num_bins_per_process = bin_counts / n_proc;
+  int start_own_bins = num_bins_per_process * rank;
+  int end_own_bins = num_bins_per_process * (rank + 1);
 
   if (rank == n_proc - 1)
-    my_bins_end = binCount;
+    end_own_bins = bin_counts;
 
   //
   //  simulate a number of time steps
@@ -167,12 +163,19 @@ int main(int argc, char **argv) {
     dmin = 1.0;
     davg = 0.0;
 
-    // compute local forces
-    for (int i = my_bins_start; i < my_bins_end; ++i) {
-      for (int j = 0; j < binCount; ++j) {
-        force_compute_for_bins(bins, i, j, dmin, davg, navg);
+    // compute forces in the bins
+    int i = start_own_bins;
+    while (i < end_own_bins){
+      for (int j = 0; j < bin_counts; ++j) {
+        compute_force_in_eache_bins(bins, i, j, dmin, davg, navg);
       }
+      ++i;
     }
+//    for (int i = start_own_bins; i < end_own_bins; ++i) {
+//      for (int j = 0; j < bin_counts; ++j) {
+//        compute_force_in_eache_bins(bins, i, j, dmin, davg, navg);
+//      }
+//    }
 
     if (find_option(argc, argv, "-no") == -1) {
       MPI_Reduce(&davg, &rdavg, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
@@ -189,168 +192,231 @@ int main(int argc, char **argv) {
     }
 
     // move, but not rebin
-    bin_t local_move;
-    bin_t remote_move;
+    bin_t move_locally;
+    bin_t move_remotely;
 
-    for (int i = my_bins_start; i < my_bins_end; ++i) {
-      for (int j = 0; j < binCount; ++j) {
-        bin_t &bin = bins[i * binCount + j];
+    int m = start_own_bins;
+    while (m < end_own_bins ){
+      for (int j = 0; j < bin_counts; ++j) {
+        bin_t &bin = bins[i * bin_counts + j];
         int tail = bin.size(), k = 0;
+
         for (; k < tail;) {
           move(bin[k]);
-          int x = int(bin[k].x / binSize);
-          int y = int(bin[k].y / binSize);
-          if (my_bins_start <= x && x < my_bins_end) {
+          int x = int(bin[k].x / bin_size);
+          int y = int(bin[k].y / bin_size);
+
+          if (start_own_bins <= x && x < end_own_bins) {
             if (x == i && y == j)
               ++k;
             else {
-              local_move.push_back(bin[k]);
+              move_locally.push_back(bin[k]);
               bin[k] = bin[--tail];
             }
           } else {
-            remote_move.push_back(bin[k]);
+            move_remotely.push_back(bin[k]);
             bin[k] = bin[--tail];
           }
         }
         bin.resize(k);
       }
+      m++;
     }
 
-    for (int i = 0; i < local_move.size(); ++i) {
-      push_particles_in_bin(local_move[i], bins);
+//    for (int i = start_own_bins; i < end_own_bins; ++i) {
+//      for (int j = 0; j < bin_counts; ++j) {
+//        bin_t &bin = bins[i * bin_counts + j];
+//        int tail = bin.size(), k = 0;
+//        for (; k < tail;) {
+//          move(bin[k]);
+//          int x = int(bin[k].x / bin_size);
+//          int y = int(bin[k].y / bin_size);
+//          if (start_own_bins <= x && x < end_own_bins) {
+//            if (x == i && y == j)
+//              ++k;
+//            else {
+//              move_locally.push_back(bin[k]);
+//              bin[k] = bin[--tail];
+//            }
+//          } else {
+//            move_remotely.push_back(bin[k]);
+//            bin[k] = bin[--tail];
+//          }
+//        }
+//        bin.resize(k);
+//      }
+//    }
+
+    for (int i = 0; i < move_locally.size(); ++i) {
+      push_particles_in_bin(move_locally[i], bins);
     }
 
     if (rank != 0) {
-      for (int i = my_bins_start - 1, j = 0; j < binCount; ++j) {
-        bin_t &bin = bins[i * binCount + j];
+      for (int i = start_own_bins - 1, j = 0; j < bin_counts; ++j) {
+        bin_t &bin = bins[i * bin_counts + j];
         bin.clear();
       }
-      for (int i = my_bins_start, j = 0; j < binCount; ++j) {
-        bin_t &bin = bins[i * binCount + j];
-        remote_move.insert(remote_move.end(), bin.begin(), bin.end());
+
+      for (int i = start_own_bins, j = 0; j < bin_counts; ++j) {
+        bin_t &bin = bins[i * bin_counts + j];
+        move_remotely.insert(move_remotely.end(), bin.begin(), bin.end());
         bin.clear();
       }
     }
 
     if (rank != n_proc - 1) {
-      for (int i = my_bins_end, j = 0; j < binCount; ++j) {
-        bin_t &bin = bins[i * binCount + j];
+      for (int i = end_own_bins, j = 0; j < bin_counts; ++j) {
+        bin_t &bin = bins[i * bin_counts + j];
         bin.clear();
       }
-      for (int i = my_bins_end - 1, j = 0; j < binCount; ++j) {
-        bin_t &bin = bins[i * binCount + j];
-        remote_move.insert(remote_move.end(), bin.begin(), bin.end());
+      for (int i = end_own_bins - 1, j = 0; j < bin_counts; ++j) {
+        bin_t &bin = bins[i * bin_counts + j];
+        move_remotely.insert(move_remotely.end(), bin.begin(), bin.end());
         bin.clear();
       }
     }
-    bin_t incoming_move;
-    int send_count = remote_move.size();
-    int recv_counts[n_proc];
 
-    MPI_Gather(&send_count, 1, MPI_INT, recv_counts, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    bin_t move_increment;
 
-    // now root knows recv_counts
-    int displs[n_proc];
-    int total_num = 0;
+    int send_count = move_remotely.size();
+    int receive_count[n_proc];
+
+    MPI_Gather(&send_count, 1, MPI_INT, receive_count, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+    // Init count
+    int displace[n_proc];
+    int total_count = 0;
 
     if (rank == 0) {
-      displs[0] = 0;
-      for (int i = 1; i < n_proc; ++i) {
-        displs[i] = displs[i - 1] + recv_counts[i - 1];
+      displace[0] = 0;
+
+      int i = 0;
+      while (i < n_proc ){
+        displace[i] = displace[i - 1] + receive_count[i - 1];
+
+        ++i;
       }
-      total_num = recv_counts[n_proc - 1] + displs[n_proc - 1];
-      incoming_move.resize(total_num);
+//      for (int i = 1; i < n_proc; ++i) {
+//        displace[i] = displace[i - 1] + receive_count[i - 1];
+//      }
+      total_count = receive_count[n_proc - 1] + displace[n_proc - 1];
+      move_increment.resize(total_count);
     }
 
-    // now root knows total_num.
-    MPI_Gatherv(remote_move.data(), send_count, PARTICLE,
-                incoming_move.data(), recv_counts, displs, PARTICLE,
+
+    // Total count
+    MPI_Gatherv(move_remotely.data(), send_count, PARTICLE,
+                move_increment.data(), receive_count, displace, PARTICLE,
                 0, MPI_COMM_WORLD);
 
-    vector <bin_t> scatter_particles;
-    scatter_particles.resize(n_proc);
+    vector <bin_t> particles_positions;
+    particles_positions.resize(n_proc);
 
     if (rank == 0) {
-      for (int i = 0; i < incoming_move.size(); ++i) {
-        int x = int(incoming_move[i].x / binSize);
+      for (int i = 0; i < move_increment.size(); ++i) {
+        int x = int(move_increment[i].x / bin_size);
 
-        assert(incoming_move[i].x >= 0 && incoming_move[i].y >= 0 &&
-               incoming_move[i].x <= gridSize && incoming_move[i].y <= gridSize);
+//        assert(move_increment[i].x >= 0 && move_increment[i].y >= 0 &&
+//               move_increment[i].x <= grid_size && move_increment[i].y <= grid_size);
 
-        int who = min(x / x_bins_per_proc, n_proc - 1);
-        scatter_particles[who].push_back(incoming_move[i]);
+        int particle_id = min(x / num_bins_per_process, n_proc - 1);
+        particles_positions[particle_id].push_back(move_increment[i]);
 
-        int row = x % x_bins_per_proc;
-        if (row == 0 && who != 0)
-          scatter_particles[who - 1].push_back(incoming_move[i]);
-        if (row == x_bins_per_proc - 1 && who != n_proc - 1)
-          scatter_particles[who + 1].push_back(incoming_move[i]);
+        int row_nums = x % num_bins_per_process;
+        if (row_nums == 0 && particle_id != 0)
+          particles_positions[particle_id - 1].push_back(move_increment[i]);
+
+        if (row_nums == num_bins_per_process - 1 && particle_id != n_proc - 1)
+          particles_positions[particle_id + 1].push_back(move_increment[i]);
       }
-      for (int i = 0; i < n_proc; ++i) {
-        recv_counts[i] = scatter_particles[i].size();
+
+      int i = 0;
+      while (i < n_proc){
+        receive_count[i] = particles_positions[i].size();
+
+        ++i;
       }
-      displs[0] = 0;
-      for (int i = 1; i < n_proc; ++i) {
-        displs[i] = displs[i - 1] + recv_counts[i - 1];
+//      for (int i = 0; i < n_proc; ++i) {
+//        receive_count[i] = particles_positions[i].size();
+//      }
+
+      displace[0] = 0;
+      int j = 1;
+      while (j < n_proc){
+        displace[i] = displace[i - 1] + receive_count[i - 1];
+
+        ++j;
+
       }
+//      for (int i = 1; i < n_proc; ++i) {
+//        displace[i] = displace[i - 1] + receive_count[i - 1];
+//      }
     }
     send_count = 0;
-    MPI_Scatter(recv_counts, 1, MPI_INT, &send_count, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Scatter(receive_count, 1, MPI_INT, &send_count, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
-    bin_t outgoing_move;
-    outgoing_move.resize(send_count);
+    bin_t move_reduction;
+    move_reduction.resize(send_count);
 
-    bin_t scatter_particles_flatten;
-    for (int i = 0; i < scatter_particles.size(); ++i) {
-      scatter_particles_flatten.insert(scatter_particles_flatten.end(),
-                                       scatter_particles[i].begin(), scatter_particles[i].end());
+    bin_t particles_in_bins;
+    for (int i = 0; i < particles_positions.size(); ++i) {
+      particles_in_bins.insert(particles_in_bins.end(),
+                               particles_positions[i].begin(), particles_positions[i].end());
     }
 
-    MPI_Scatterv(scatter_particles_flatten.data(), recv_counts, displs, PARTICLE,
-                 outgoing_move.data(), send_count, PARTICLE, 0, MPI_COMM_WORLD);
+    MPI_Scatterv(particles_in_bins.data(), receive_count, displace, PARTICLE,
+                 move_reduction.data(), send_count, PARTICLE, 0, MPI_COMM_WORLD);
 
-    for (int i = 0; i < send_count; ++i) {
-      particle_t &p = outgoing_move[i];
-      assert(p.x >= 0 && p.y >= 0 && p.x <= gridSize && p.y <= gridSize);
+    int l = 0;
+    while (l < send_count){
+      particle_t &p = move_reduction[i];
+      assert(p.x >= 0 && p.y >= 0 && p.x <= grid_size && p.y <= grid_size);
       push_particles_in_bin(p, bins);
-    }
-  }
-  simulation_time = read_timer() - simulation_time;
 
-  if (rank == 0) {
-    printf("n = %d, simulation time = %g seconds", n, simulation_time);
-
-    if (find_option(argc, argv, "-no") == -1) {
-      if (nabsavg) absavg /= nabsavg;
-      //
-      //  -The minimum distance absmin between 2 particles during the run of the simulation
-      //  -A Correct simulation will have particles stay at greater than 0.4 (of cutoff) with typical values between .7-.8
-      //  -A simulation where particles don't interact correctly will be less than 0.4 (of cutoff) with typical values between .01-.05
-      //
-      //  -The average distance absavg is ~.95 when most particles are interacting correctly and ~.66 when no particles are interacting
-      //
-      printf(", absmin = %lf, absavg = %lf", absmin, absavg);
-      if (absmin < 0.4) printf("\nThe minimum distance is below 0.4 meaning that some particle is not interacting");
-      if (absavg < 0.8) printf("\nThe average distance is below 0.8 meaning that most particles are not interacting");
+      ++l;
     }
-    printf("\n");
+//    for (int i = 0; i < send_count; ++i) {
+//      particle_t &p = move_reduction[i];
+//      assert(p.x >= 0 && p.y >= 0 && p.x <= grid_size && p.y <= grid_size);
+//      push_particles_in_bin(p, bins);
+//    }
+    }
+    simulation_time = read_timer() - simulation_time;
+
+    if (rank == 0) {
+      printf("n = %d, simulation time = %g seconds", n, simulation_time);
+
+      if (find_option(argc, argv, "-no") == -1) {
+        if (nabsavg) absavg /= nabsavg;
+        //
+        //  -The minimum distance absmin between 2 particles during the run of the simulation
+        //  -A Correct simulation will have particles stay at greater than 0.4 (of cutoff) with typical values between .7-.8
+        //  -A simulation where particles don't interact correctly will be less than 0.4 (of cutoff) with typical values between .01-.05
+        //
+        //  -The average distance absavg is ~.95 when most particles are interacting correctly and ~.66 when no particles are interacting
+        //
+        printf(", absmin = %lf, absavg = %lf", absmin, absavg);
+        if (absmin < 0.4) printf("\nThe minimum distance is below 0.4 meaning that some particle is not interacting");
+        if (absavg < 0.8) printf("\nThe average distance is below 0.8 meaning that most particles are not interacting");
+      }
+      printf("\n");
+
+      //
+      // Printing summary data
+      //
+      if (fsum)
+        fprintf(fsum, "%d %d %g\n", n, n_proc, simulation_time);
+    }
 
     //
-    // Printing summary data
+    //  release resources
     //
     if (fsum)
-      fprintf(fsum, "%d %d %g\n", n, n_proc, simulation_time);
+      fclose(fsum);
+    if (fsave)
+      fclose(fsave);
+
+    MPI_Finalize();
+
+    return 0;
   }
-
-  //
-  //  release resources
-  //
-  if (fsum)
-    fclose(fsum);
-  if (fsave)
-    fclose(fsave);
-
-  MPI_Finalize();
-
-  return 0;
-}
